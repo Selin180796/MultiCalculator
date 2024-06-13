c41827d914e685a4d3f7b40d2eaf1e4a0b505637
Selin <schrist9@mycambrian.ca>
2024-06-12 22:11:51 -0400

    Implemented Numeric Buttons

--- composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -3,10 +3,16 @@ package org.example.multicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +35,15 @@ fun CalculatorContent() {
     val displayState = remember { mutableStateOf("0") }
 
     Surface(color = MaterialTheme.colors.background) {
-        CalcDisplay(displayState)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CalcDisplay(displayState)
+            Spacer(modifier = Modifier.height(16.dp))
+            CalcNumericButton(1, displayState)
+        }
     }
 }
 
@@ -41,4 +55,14 @@ fun CalcDisplay(displayState: MutableState<String>) {
         modifier = Modifier.padding(16.dp),
         color = Color.Black
     )
+}
+
+@Composable
+fun CalcNumericButton(number: Int, displayState: MutableState<String>) {
+    Button(
+        onClick = { displayState.value = number.toString() },
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Text(text = number.toString())
+    }
 }
