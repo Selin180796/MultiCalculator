32604dcab832627fbbc5fd2a4540e6841c931be7
Selin <schrist9@mycambrian.ca>
2024-06-21 21:32:32 -0400

    another message changed

--- composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -1,208 +1,120 @@
 package org.example.multicalculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            CalculatorContent()
+            CalcView()
         }
     }
 }
 
-@Preview
 @Composable
-fun AppAndroidPreview() {
-    CalculatorContent()
-}
+fun CalcView() {
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@Composable
-fun CalculatorContent() {
-    val displayState = remember { mutableStateOf("0") }
+        CalcDisplay(display = displayText)
 
-    Scaffold(
-        backgroundColor = Color.LightGray,
-        content = {
-            CalcView(displayState)
-        }
-    )
-}
-
-@Composable
-fun CalcView(displayState: MutableState<String>) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // Display Area
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(120.dp)
-                .padding(8.dp)
-                .background(Color.White)
-        ) {
-            CalcDisplay(displayState)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Numeric Buttons
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            // Rows of Numeric Buttons
-            for (i in 2 downTo 0) {
-                CalcRow(startNum = i * 3 + 1, numButtons = 3, displayState = displayState)
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-
-            // Zero, Equals, and Clear Button Row
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                CalcNumericButton(number = 0, displayState = displayState)
-                CalcEqualsButton(displayState = displayState)
-            }
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        // Operation Buttons
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.padding(top = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            CalcOperationButton(operation = "/", displayState = displayState)
-            CalcOperationButton(operation = "*", displayState = displayState)
-            CalcOperationButton(operation = "-", displayState = displayState)
-            CalcOperationButton(operation = "+", displayState = displayState)
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                for (i in 7 downTo 1 step 3) {
+                    CalcRow(onPress = { numberPress(it) }, startNum = i, numButtons = 3)
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    CalcNumericButton(number = 0, onPress = { numberPress(it) })
+                    CalcEqualsButton(onPress = { equalsPress() })
+                }
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                val operations = listOf("+", "-", "*", "/")
+                operations.forEach { op ->
+                    CalcOperationButton(operation = op, onPress = { operationPress(it) })
+                }
+            }
         }
     }
 }
 
 @Composable
-fun CalcRow(startNum: Int, numButtons: Int, displayState: MutableState<String>) {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-        for (i in startNum until startNum + numButtons) {
-            CalcNumericButton(number = i, displayState = displayState)
+fun CalcRow(onPress: (number: Int) -> Unit, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
+    Row(
+        modifier = Modifier.padding(0.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        for (num in startNum until endNum) {
+            CalcNumericButton(number = num, onPress = onPress)
         }
     }
 }
 
 @Composable
-fun CalcDisplay(displayState: MutableState<String>) {
+fun CalcDisplay(display: MutableState<String>) {
     Text(
-        text = displayState.value,
-        style = MaterialTheme.typography.h4,
+        text = display.value,
+        fontSize = 32.sp,
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        color = Color.Black,
-        textAlign = TextAlign.End
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(5.dp)
     )
 }
 
 @Composable
-fun CalcNumericButton(number: Int, displayState: MutableState<String>, span: Int = 1) {
+fun CalcNumericButton(number: Int, onPress: (number: Int) -> Unit) {
     Button(
-        onClick = { displayState.value = if (displayState.value == "0") "$number" else "${displayState.value}$number" },
+        onClick = { onPress(number) },
         modifier = Modifier
-            .padding(4.dp)
-            .width(if (span == 2) 160.dp else 80.dp)
-            .height(80.dp)
-            .background(color = Color.LightGray),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Blue,
-            contentColor = Color.White
-        )
+            .padding(0.dp)
+            .size(80.dp)
     ) {
-        Text(text = "$number", style = MaterialTheme.typography.body1)
+        Text(text = number.toString(), fontSize = 24.sp)
     }
 }
 
 @Composable
-fun CalcOperationButton(operation: String, displayState: MutableState<String>) {
+fun CalcOperationButton(operation: String, onPress: (operation: String) -> Unit) {
     Button(
-        onClick = { displayState.value = "${displayState.value} $operation " },
+        onClick = { onPress(operation) },
         modifier = Modifier
-            .padding(4.dp)
-            .width(80.dp)
-            .height(80.dp)
-            .background(color = Color.LightGray),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Blue,
-            contentColor = Color.White
-        )
+            .padding(0.dp)
+            .size(80.dp)
     ) {
-        Text(text = operation, style = MaterialTheme.typography.body1)
+        Text(text = operation, fontSize = 24.sp)
     }
 }
 
 @Composable
-fun CalcEqualsButton(displayState: MutableState<String>) {
+fun CalcEqualsButton(onPress: () -> Unit) {
     Button(
-        onClick = {
-            try {
-                val result = evaluateExpression(displayState.value)
-                displayState.value = result.toString()
-            } catch (e: ArithmeticException) {
-                displayState.value = "Error"
-            }
-        },
+        onClick = { onPress() },
         modifier = Modifier
-            .padding(4.dp)
-            .width(80.dp)
-            .height(80.dp)
-            .background(color = Color.LightGray),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Blue,
-            contentColor = Color.White
-        )
+            .padding(0.dp)
+            .size(80.dp)
     ) {
-        Text(text = "=", style = MaterialTheme.typography.body1)
+        Text(text = "=", fontSize = 24.sp)
     }
-}
-
-fun evaluateExpression(expression: String): Int {
-    val parts = expression.split(" ")
-    val left = parts[0].toInt()
-    val right = parts[2].toInt()
-    val operator = parts[1]
-
-    return when (operator) {
-        "+" -> left + right
-        "-" -> left - right
-        "*" -> left * right
-        "/" -> left / right
-        else -> throw IllegalArgumentException("Unidentified operator")
-    }
-
-
 }
